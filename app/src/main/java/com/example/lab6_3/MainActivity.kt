6686a323878db1d36f0b4697928f0a48badee7ad
Igor <i.galaxy00@gmail.com>
2021-12-18 12:52:30 +0300

    final?

--- app/src/main/java/com/example/lab6_3/MainActivity.kt
+++ app/src/main/java/com/example/lab6_3/MainActivity.kt
@@ -5,34 +5,34 @@ import android.os.Bundle
 import android.graphics.Bitmap
 import android.util.Log
 import android.widget.ImageView
-import androidx.lifecycle.MutableLiveData
-import java.util.concurrent.ExecutorService
+
+import android.graphics.BitmapFactory
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.repeatOnLifecycle
+
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.*
+import java.net.URL
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var executor: ExecutorService
-    private lateinit var imageView : ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        imageView  = findViewById(R.id.imageView)
-        val viewModel = MainViewModel()
+        val image = findViewById<ImageView>(R.id.imageView)
 
-        if (imageView.drawable == null) {
-            viewModel.loadImage()
-        }
-
-        viewModel.bitmapData.observe(this) {
-            if (it != null) {
-                runOnUiThread {
-                    imageView.setImageBitmap(it)
+        lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                withContext(Dispatchers.IO) {
+                    val bitmap = BitmapFactory.decodeStream(
+                        URL("https://images-ext-2.discordapp.net/external/nd_xyZ_HZ2L38OfAWNkS2tlqpIKcstQ5yxTpgV61I5Y/https/i.ibb.co/BPDWVhj/unnamed.jpg")
+                            .openConnection().inputStream
+                    )
+                    withContext(Dispatchers.Main) {
+                        image.setImageBitmap(bitmap)
+                    }
                 }
             }
         }
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        executor.shutdown()
     }
 }
